97fe9cbf72a650e554a721d04a23ed09a7eab2e6
Camry <adgjls2008@gmail.com>
2026-07-24 12:45:12 +0530

    some bug fix

--- Server/backend/Train_model/training.py
+++ Server/backend/Train_model/training.py
@@ -10,15 +10,9 @@ from keras import Sequential
 from keras.layers import Dense, Input
 
 
-# 1. Load dataset
-
 df = pd.read_csv(
     "Crop_recommendation.csv"
 )
-
-
-# 2. Select features
-
 X = df[
     [
         "N",
@@ -31,21 +25,12 @@ X = df[
     ]
 ]
 
-
-
-
 y = df["label"]
-
-
 
 
 label_encoder = LabelEncoder()
 
 y_encoded = label_encoder.fit_transform(y)
-
-
-# 5. Split dataset
-
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y_encoded,
@@ -54,8 +39,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y_encoded
 )
 
-
-# 6. Scale features
 
 scaler = StandardScaler()
 
@@ -66,23 +49,18 @@ X_train_scaled = scaler.fit_transform(
 X_test_scaled = scaler.transform(
     X_test
 )
-
-
-# 7. Create ANN
-
 model = Sequential([
     Input(shape=(7,)),
-
     Dense(
         128,
         activation="relu"
-    ),
 
+    ),
     Dense(
         64,
         activation="relu"
-    ),
 
+    ),
     Dense(
         32,
         activation="relu"
@@ -92,19 +70,15 @@ model = Sequential([
         len(label_encoder.classes_),
         activation="softmax"
     )
+
 ])
 
-
-# 8. Compile
 
 model.compile(
     optimizer="adam",
     loss="sparse_categorical_crossentropy",
     metrics=["accuracy"]
 )
-
-
-# 9. Train
 
 model.fit(
     X_train_scaled,
@@ -113,10 +87,6 @@ model.fit(
     batch_size=32,
     validation_split=0.2
 )
-
-
-# 10. Evaluate
-
 test_loss, test_accuracy = model.evaluate(
     X_test_scaled,
     y_test
@@ -126,17 +96,10 @@ print(
     "Test Accuracy:",
     test_accuracy
 )
-
-
-# 11. Create model directory
-
 os.makedirs(
     "../models",
     exist_ok=True
 )
-
-
-# 12. Save trained model
 model_path=os.path.abspath(
     "../models/crop_model.keras"
 )
@@ -154,25 +117,16 @@ print(label_path)
 model.save(
     model_path
 )
-
-
-# 13. Save scaler
-
 joblib.dump(
     scaler,
     scaler_path
 )
-
-
-# 14. Save label encoder
 
 joblib.dump(
     label_encoder,
     label_path
     
 )
-
-
 print(
     "Model, scaler and label encoder saved successfully."
 )
--- Server/backend/src/controller/fertilizer_suggestion.py
+++ Server/backend/src/controller/fertilizer_suggestion.py
@@ -6,20 +6,19 @@ def fertilizer_suggestion(N, P, K):
         suggestions.append(
             "Nitrogen is low. Consider nitrogen-rich fertilizer."
         )
-
     if P < 40:
         suggestions.append(
             "Phosphorus is low. Consider phosphate fertilizer."
         )
-
     if K < 40:
         suggestions.append(
             "Potassium is low. Consider potassium-rich fertilizer."
         )
-
     if len(suggestions) == 0:
         suggestions.append(
             "NPK levels appear to be adequate."
         )
 
     return suggestions
+
+
