30f18c0624d7069580be2a2f22ccd2ae08fcc6a7
Camry <adgjls2008@gmail.com>
2026-07-25 19:37:27 +0530

    changed src

--- Server/backend/src/index.py
+++ Server/backend/src/index.py
@@ -1,6 +1,6 @@
 from flask import Flask,jsonify
 from flask_cors import CORS
-from routes.crop_routes import crop_routes
+from src.routes.crop_routes import crop_routes
 
 def create_app():
     app=Flask(__name__)
