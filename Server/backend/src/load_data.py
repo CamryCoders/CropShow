12d5c79342bb2904e3c85b7669b3feb2564c9210
Camry <adgjls2008@gmail.com>
2026-07-25 21:00:26 +0530

    Time checking in model

--- Server/backend/src/load_data.py
+++ Server/backend/src/load_data.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import joblib
 import tensorflow as tf
-
+import time
+start = time.time()
 BASE_DIR=Path(__file__).resolve().parent.parent
 print("Base_dir",BASE_DIR)
 MODEL_PATH = BASE_DIR / "models" / "crop_model.keras"
@@ -13,10 +14,12 @@ print("Size:", MODEL_PATH.stat().st_size, "bytes")
 model = tf.keras.models.load_model(
     BASE_DIR/"models"/"crop_model.keras"
 )
+print("After scaling:", time.time() - start)
 scaler = joblib.load(
     BASE_DIR/"models"/"scaler.pkl"
 )
-
+print("After prediction:", time.time() - start)
 label_encoder = joblib.load(
     BASE_DIR/"models"/"label_encoder.pkl"
 )
+print("Finished:", time.time() - start)
